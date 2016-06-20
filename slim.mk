416f7574a747f4884728be1cc4264c89b5450817
Matt Mower <mowerm@gmail.com>
2016-06-19 20:25:09 -0500

    m7: Update fingerprint to LMY47O.H18
    Change-Id: I489724fdb4aa8dc3402352411e04e92ac7bbffb2

--- slim.mk
+++ slim.mk
@@ -21,10 +21,10 @@ $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Device naming
-PRODUCT_NAME := m7
+PRODUCT_NAME := slim_m7
 
 # Override build props
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="htc/m7_google/m7:5.0.1/LRX22C.H3/434277:user/release-keys" \
-    BUILD_ID=LRX22C.H3 \
-    PRIVATE_BUILD_DESC="5.11.1700.3 CL434277 release-keys"
+    BUILD_FINGERPRINT="htc/m7_google/m7:5.1/LMY47O.H18/666675:user/release-keys" \
+    BUILD_ID=LMY47O.H18 \
+    PRIVATE_BUILD_DESC="6.04.1700.18 CL536258 release-keys"
